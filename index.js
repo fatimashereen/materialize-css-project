a9adb04000b05e10322c670b31acc7d6a2b86c55
Fatima Shereen <fatimabashir.2016@hotmail.com>
2019-03-24 06:21:47 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+
+
+
+    $('.slider').slider({
+    fullWidth:true,
+    indicators:false,
+     height:500
+
+    });
+
+    
+  
+        
+
+
+
